3ff962e788bc1bf5e70b0f541767ec8d1287cc74
Manuelly Suzik <manuellysuzik@gmail.com>
2023-06-06 00:45:15 -0300

    first commit

---
+++ cmd/app.go
@@ -0,0 +1,10 @@
+package cmd
+
+import "fmt"
+
+func ExecuteApp() {
+	fmt.Println("O App iniciou")
+}
+func ExecuteOther() {
+	fmt.Println("outra aplicação")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/manuellysuzik/hotel.git
+
+go 1.20
