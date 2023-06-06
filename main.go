ef327c57193c9c14523de2ed06fdfe92d5728fa8
Manuelly Suzik <manuellysuzik@gmail.com>
2023-06-06 00:45:22 -0300

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package hotel
+package main
+
+import (
+	"flag"
+	"github.com/manuellysuzik/hotel.git/cmd"
+)
+
+const (
+	startApplication = "app"
+	other            = "other"
+)
+
+var command string
+
+func main() {
+
+	flag.StringVar(&command, "c", startApplication, startApplication)
+
+	flag.Parse()
+
+	switch command {
+	case startApplication:
+		cmd.Start
+	case other:
+		cmd.ExecuteOther()
+	}
+}
